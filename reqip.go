bb1cfd485f5c922451fb84d3aa5dc295c417375c
Gábor Görzsöny <gabor@gorzsony.com>
2020-11-02 02:20:33 +0100

    Fixed bugs

--- reqip.go
+++ reqip.go
@@ -19,22 +19,7 @@ func getClientIPFromXForwardedFor(header string) string {
 
 	// x-forwarded-for may return multiple IP addresses in the format
 	// @see https://en.wikipedia.org/wiki/X-Forwarded-For#Format
-	proxies := strings.Split(header, ", ")
-
-	var ips []string
-
-	if len(proxies) > 0 {
-		for _, proxy := range proxies {
-			ip := proxy
-			// make sure we only use this if it's ipv4 (ip:port)
-			if strings.Contains(ip, ":") {
-				splitted := strings.Split(ip, ":")
-				ips = append(ips, splitted[0])
-				continue
-			}
-			ips = append(ips, ip)
-		}
-	}
+	ips := strings.Split(header, ", ")
 
 	// Sometimes IP addresses in this header can be 'unknown' (http://stackoverflow.com/a/11285650).
 	// Therefore taking the left-most IP address that is not unknown
@@ -54,12 +39,12 @@ func GetClientIP(r *http.Request) string {
 
 	if len(headers) > 0 {
 		checklist := []string{
-			"x-client-ip", // Standard headers used by Amazon EC2, Heroku, and others.
-			"x-forwarded-for", // Load-balancers (AWS ELB) or proxies.
-			"cf-connecting-ip", // @see https://support.cloudflare.com/hc/en-us/articles/200170986-How-does-Cloudflare-handle-HTTP-Request-headers-
-			"fastly-client-ip", // Fastly and Firebase hosting header (When forwared to cloud function)
-			"true-client-ip", // Akamai and Cloudflare: True-Client-IP.
-			"x-real-ip", // Default nginx proxy/fcgi; alternative to x-forwarded-for, used by some proxies.
+			"x-client-ip",         // Standard headers used by Amazon EC2, Heroku, and others.
+			"x-forwarded-for",     // Load-balancers (AWS ELB) or proxies.
+			"cf-connecting-ip",    // @see https://support.cloudflare.com/hc/en-us/articles/200170986-How-does-Cloudflare-handle-HTTP-Request-headers-
+			"fastly-client-ip",    // Fastly and Firebase hosting header (When forwared to cloud function)
+			"true-client-ip",      // Akamai and Cloudflare: True-Client-IP.
+			"x-real-ip",           // Default nginx proxy/fcgi; alternative to x-forwarded-for, used by some proxies.
 			"x-cluster-client-ip", // (Rackspace LB and Riverbed's Stingray) http://www.rackspace.com/knowledge_center/article/controlling-access-to-linux-cloud-sites-based-on-the-client-ip-address
 			"x-forwarded",
 			"forwarded-for",
@@ -80,9 +65,6 @@ func GetClientIP(r *http.Request) string {
 		}
 	}
 
-	if ip := r.RemoteAddr; isIP(ip) {
-		return ip
-	}
-
-	return ""
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	return ip
 }
